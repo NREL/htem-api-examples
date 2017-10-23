de5bcadbfe0b90ee90bea435389001a7f29f33a8
meschw04 <meschw04@cardmail.louisville.edu>
2017-10-23 00:25:04 -0400

    Minor change...

---
+++ lib/sample.py
@@ -0,0 +1,14 @@
+# Written by Marcus Schwarting in collaboration with the National Renewable
+# Energy Laboratories. For further information please see the website 
+# [insert website name here] as well as the recent publication on this work,
+# [insert reference to upcoming paper here]. The goal of this project is to
+# further the objectives laid out in the strategic objectives for the
+# Materials Genome Initiative.
+# 
+# For further information on this code project, please email:
+# Marcus Schwarting ==> marcus.schwarting@nrel.gov
+
+#POSITION SPECIFIC!
+
+class Sample:
+    
